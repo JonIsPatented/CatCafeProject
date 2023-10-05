241663ebe3f634fd535ba0a2e6a5229fdaaad7c8
JonIsPatented <rockerjon01@gmail.com>
2023-10-05 15:01:10 -0400

    Improved Menu.

--- CatCafeProject/Menu.cpp
+++ CatCafeProject/Menu.cpp
@@ -1,26 +1,111 @@
 #include "Menu.h"
 
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+namespace cc
+{
+    class MenuOption
+    {
+    public:
+        MenuOption(const std::string& stringRepresentation)
+            : m_stringRepresentation(stringRepresentation) {}
+        virtual void Activate(cc::Menu&) const = 0;
+        const std::string& GetStringRepresentation() const;
+    protected:
+        cc::CatRegistry& GetCatRegistry(cc::Menu&) const;
+    private:
+        std::string m_stringRepresentation;
+    };
+
+    class AddCatMenuOption : public MenuOption
+    {
+    public:
+        AddCatMenuOption() : MenuOption("Add Cat") {}
+        void Activate(cc::Menu& callingMenu) const
+        {
+            GetCatRegistry(callingMenu).PromptCreate();
+        }
+    };
+
+    class RemoveCatMenuOption : public MenuOption
+    {
+    public:
+        RemoveCatMenuOption() : MenuOption("Remove Cat") {}
+        void Activate(cc::Menu& callingMenu) const
+        {
+            // TODO
+        }
+    };
+
+    class DisplayCatsMenuOption : public MenuOption
+    {
+    public:
+        DisplayCatsMenuOption() : MenuOption("Display Cats") {}
+        void Activate(cc::Menu& callingMenu) const
+        {
+            GetCatRegistry(callingMenu).DisplayAllCats();
+        }
+    };
+
+    class ExitMenuOption : public MenuOption
+    {
+    public:
+        ExitMenuOption() : MenuOption("Exit") {}
+        void Activate(cc::Menu& callingMenu) const
+        {
+            callingMenu.Stop();
+        }
+    };
+}
+
 void cc::Menu::Run()
 {
-	// TODO
+    while (m_shouldContinue)
+    {
+        PrintMenu();
+
+    }
 }
 
-cc::CatRegistry& cc::MenuOption::GetCatRegistry(Menu& callingMenu) const
+void cc::Menu::PrintMenu() const
 {
-	return callingMenu.m_catRegistry;
+    std::cout << "Main Menu:\n\n----------------------------------\n" << std::endl;
+    for (int i = 0; i < m_menuOptions.size(); i++)
+    {
+        std::stringstream indexMarker;
+        indexMarker << i + 1 << '.';
+        std::cout << std::setw(6) << std::left
+            << indexMarker.str()
+            << std::setw(28) << std::right
+            << m_menuOptions[i]->GetStringRepresentation()
+            << "\n";
+    }
+    std::cout << "\n----------------------------------\n" << std::endl;
 }
 
-void cc::AddCatMenuOption::Activate(Menu& callingMenu) const
+void cc::Menu::Stop()
 {
-	GetCatRegistry(callingMenu).PromptCreate();
+    m_shouldContinue = false;
 }
 
-void cc::RemoveCatMenuOption::Activate(Menu& callingMenu) const
+cc::CatRegistry& cc::MenuOption::GetCatRegistry(cc::Menu& callingMenu) const
 {
-	// TODO
+    return callingMenu.m_catRegistry;
 }
 
-void cc::DisplayCatsMenuOption::Activate(Menu& callingMenu) const
+const std::string& cc::MenuOption::GetStringRepresentation() const
 {
-	GetCatRegistry(callingMenu).DisplayAllCats();
+    return m_stringRepresentation;
+}
+
+cc::Menu::Menu()
+    : m_shouldContinue(true)
+{
+    m_menuOptions.emplace_back(std::make_unique<AddCatMenuOption>());
+    m_menuOptions.emplace_back(std::make_unique<RemoveCatMenuOption>());
+    m_menuOptions.emplace_back(std::make_unique<DisplayCatsMenuOption>());
+    m_menuOptions.emplace_back(std::make_unique<ExitMenuOption>());
 }
--- CatCafeProject/Menu.h
+++ CatCafeProject/Menu.h
@@ -8,42 +8,27 @@
 
 namespace cc
 {
-	class Menu
-	{
-	public:
-		void Run();
-	private:
-		friend class MenuOption;
-	private:
-		std::vector<std::unique_ptr<MenuOption>> m_menuOptions;
-		CatRegistry m_catRegistry;
-	};
-
-	class MenuOption
-	{
-	public:
-		virtual void Activate(Menu&) const = 0;
-	protected:
-		CatRegistry& GetCatRegistry(Menu&) const;
-	};
-
-	class AddCatMenuOption : public MenuOption
-	{
-	public:
-		void Activate(Menu&) const;
-	};
-
-	class RemoveCatMenuOption : public MenuOption
-	{
-	public:
-		void Activate(Menu&) const;
-	};
-
-	class DisplayCatsMenuOption : public MenuOption
-	{
-	public:
-		void Activate(Menu&) const;
-	};
+    class Menu
+    {
+    public:
+        void Run();
+        void Stop();
+        void PrintMenu() const;
+    public:
+        Menu();
+        Menu(const Menu&);
+        Menu(Menu&&) noexcept;
+        ~Menu();
+    public:
+        Menu& operator=(const Menu&);
+        Menu& operator=(Menu&&) noexcept;
+    private:
+        friend class MenuOption;
+    private:
+        std::vector<std::unique_ptr<MenuOption>> m_menuOptions;
+        CatRegistry m_catRegistry;
+        bool m_shouldContinue;
+    };
 }
 
 #endif
